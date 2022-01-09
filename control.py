0c66615ee536804efbddecac86e5d3fc30fe7457
Meesum A. Qazalbash <62182585+MeesumAliQazalbash@users.noreply.github.com>
2022-01-09 18:47:14 +0500

    Game running
    AI aspect of the game is left. One more thing left, if there is no changes grid then there should be no twitch().

--- control.py
+++ control.py
@@ -1,1 +1,36 @@
-from "grid.py" import Grid
+from grid import Grid
+
+
+class Control:
+    def __init__(self, n):
+        self.g = Grid(n)
+        print(
+            """
+Commands:
+Up [U or u]
+Down [D or d]
+Left [L or l]
+Right [R or r]
+        """
+        )
+
+    def getInput(self):
+        self.g.show()
+
+        w = input("Enter the input: ")
+
+        if w.lower() == "u":
+            self.g.addUp()
+        elif w.lower() == "d":
+            self.g.addDown()
+        elif w.lower() == "l":
+            self.g.addLeft()
+        elif w.lower() == "r":
+            self.g.addRight()
+        else:
+            print("Invalid Input")
+
+
+c = Control(4)
+while True:
+    c.getInput()
